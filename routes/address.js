25e8014650031232b8296de1deb6f4e8683f888d
lakshmi <lakshmi.plr@gmail.com>
2015-11-22 23:19:26 +0530

    address id delete

--- routes/address.js
+++ routes/address.js
@@ -46,7 +46,7 @@ router.post('/update', function(req, res, next){
 	console.log("request address "+reqemail)
 	var address = require("../model/address");
 	var newadd = new address(addr);
-
+	delete newadd._id;
 	address.findOneAndUpdate(query, newadd, {upsert:true}, function(err, doc){
 	    if (err) return res.send(500, { error: err });
 	    return res.send("succesfully saved");
