d65c3b488b3e10df0a2b97cc8f7a990ea1fd6c23
lakshmi <lakshmi.plr@gmail.com>
2015-11-22 23:27:26 +0530

    id omitting

--- routes/address.js
+++ routes/address.js
@@ -46,8 +46,8 @@ router.post('/update', function(req, res, next){
 	console.log("request address "+reqemail)
 	var address = require("../model/address");
 	var newadd = new address(addr);
-	delete newadd._id;
-	address.findOneAndUpdate(query, newadd, {upsert:true}, function(err, doc){
+	delete addr._id;
+	address.findOneAndUpdate(query, addr, {upsert:true}, function(err, doc){
 	    if (err) return res.send(500, { error: err });
 	    return res.send("succesfully saved");
 	});
